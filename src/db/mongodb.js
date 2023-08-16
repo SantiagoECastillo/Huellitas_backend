5e3fb2ee8755057bb5bed0762e182a30be9ed8d8
Santiago Emanuel Castillo <castillosantiagoe1998@gmail.com>
2023-08-16 18:18:27 -0300

    Arreglado error al conectar al servidor

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -4,13 +4,13 @@ const mongoose = require("mongoose");
 const connectDb = async () =>{
     try {
         mongoose.set('strictQuery', false); //soluciona error de strictQuery
-        await mongoose.connect("mongodb://localhost:27017/Huellitas_vet", {
-            useNewUrlParse: true,
+        await mongoose.connect('mongodb://0.0.0.0:27017/Huellitas_vet', {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log("Concexion extiosa a la base de datos");
     } catch (error) {
-        console.log("error al conectar");
+        console.log(error);
     }
 }
 
