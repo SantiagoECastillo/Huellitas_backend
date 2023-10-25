842687d9d3d4d07db37f51099756b3c829b9c156
Pablo <pablomd_98@hotmail.com>
2023-10-25 19:12:05 -0300

    se agrega funcion para corregir error de cors

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ const comprobacionJwt = require("./src/middleware/comprobacionJwt");
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(cors({
-    origin: "https://veterinariahuellitasrc.netlify.app/"
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 const initApp = async () => {
