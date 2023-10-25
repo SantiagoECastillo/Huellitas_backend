067bf9c1cc70440ec73a2fc20a794845412a8f26
Pablo <pablomd_98@hotmail.com>
2023-10-25 16:22:05 -0300

    se intenta solucionar problema de cors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const comprobacionJwt = require("./src/middleware/comprobacionJwt");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: "https://veterinariahuellitasrc.netlify.app/"
+}));
 
 const PORT = process.env.PORT || 3000;
 const initApp = async () => {
