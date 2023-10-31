e08c1fa82bb6fdb8f0338548b6664cf099f2b808
Pablo <pablomd_98@hotmail.com>
2023-10-31 17:47:03 -0300

    se corrige detalles, base de datos funcionando

--- db/mongodb.js
+++ db/mongodb.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
         // mongoose.set('strictQuery', false); //soluciona error de strictQuery
         //await mongoose.connect("mongodb://127.0.0.1:27017/VeterinariaHuellitas", {
         //await mongoose.connect("mongodb://localhost:27017/VeterinariaHuellitas", {
-        await mongoose.connect("mongodb+srv://huellitas:veterinariahuellitas@veterinariahuellitas.val3vvb.mongodb.net/", {
+        await mongoose.connect("mongodb+srv://huellitas:veterinariahuellitas@veterinariahuellitas.val3vvb.mongodb.net/VeterinariaHuellitas", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
