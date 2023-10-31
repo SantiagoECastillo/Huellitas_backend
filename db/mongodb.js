4ad9b42fb85b508bbdf961b0ad081cccb0861045
Pablo <pablomd_98@hotmail.com>
2023-10-31 15:47:37 -0300

    se cambia enrutamiento de base de datos

--- db/mongodb.js
+++ db/mongodb.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
     try {
         // mongoose.set('strictQuery', false); //soluciona error de strictQuery
-        await mongoose.connect("mongodb://localhost:27017/VeterinariaHuellitas", {
+        await mongoose.connect("mongodb://127.0.0.1:27017/VeterinariaHuellitas", {
+        //await mongoose.connect("mongodb://localhost:27017/VeterinariaHuellitas", {
         // await mongoose.connect('mongodb+srv://huellitas:veterinariahuellitas@veterinariahuellitas.val3vvb.mongodb.net/VeterinariaHuellitas', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
